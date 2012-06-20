9cb61fa41e8e292349997dfafd1cfc22837d085f
Michael Niepel <michael.niepel@gmail.com>
2012-06-20 09:30:21 +0200

    a

---
+++ AR-intro/AR-intro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AR-intro
+//
+//  Created by Michael Niepel on 6/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
